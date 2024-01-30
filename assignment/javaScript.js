5aa6f65ded647b6648184db142f56a67a2c3812c
gamzazoha <70256365+gamzazoha@users.noreply.github.com>
2024-01-31 00:07:29 +0900

    Add files via upload
    test

---
+++ assignment/javaScript.js
@@ -0,0 +1,89 @@
+// toggle menu
+var toggleBtnClk=document.getElementById("drop-btn");
+
+function menu(){
+    let click=document.getElementById("drop-content");
+    click.classList.toggle("hidden");
+}
+
+toggleBtnClk.addEventListener('click', menu);
+
+var dropContent=document.getElementById("drop-content");
+var menuItems=dropContent.getElementsByTagName("a");
+
+var maxHeight=Array.from(menuItems).reduce(function(maxHeight, menuItems) {
+    return maxHeight + menuItems.offsetHeight;
+}, 0);
+
+dropContent.style.setProperty("maxHeight", maxHeight+"px")
+
+// carousel menue
+var arrow=document.getElementsByClassName("arrow");
+var page=document.getElementsByClassName("frame");
+var pageIdx=2;
+var n;
+showSlide(pageIdx);
+
+function plusSlides() {
+    if(this.classList.contains("prev")){
+        n=-1;
+    }
+    if(this.classList.contains("next")){
+        n=1;
+    }
+    showSlide(pageIdx+=n);
+}
+
+function showSlide(index) {
+    var i;
+    
+    if(!(index>page.length) && !(index<=0)) {
+        for(i=0;i<page.length;i++){
+            page[i].style.display="none";
+        }
+        page[pageIdx-1].style.display="block";
+    }
+
+    if(pageIdx>=page.length){
+        pageIdx=page.length;
+    }
+    if(pageIdx<=0){
+        pageIdx=1
+    }
+}
+
+arrow[0].addEventListener('click', plusSlides);
+arrow[1].addEventListener('click', plusSlides);
+
+// color button
+var btnClk=document.getElementsByClassName("btn");
+var area=document.getElementById("color-window");
+
+let options={
+    fill: "forwards"
+};
+
+function changeColor(event){
+    var t=event.target;
+
+    if(t.classList.contains("red")){
+        area.style.backgroundColor="rgba(255, 0, 0, 0.4)";
+        area.innerHTML="빨간 버튼입니다."
+    }
+    if(t.classList.contains("green")){
+        area.style.backgroundColor="rgba(34, 139, 34, 0.4)";
+        area.innerHTML="초록 버튼입니다."
+    }
+    if(t.classList.contains("blue")){
+        area.style.backgroundColor="rgba(0, 0, 255, 0.4)";
+        area.innerHTML="파란 버튼입니다."
+    }
+
+    area.classList.remove("pop-effect");
+    void area.offsetWidth;
+    area.classList.add("pop-effect");
+}
+
+for(var i=0; i<btnClk.length; i++){
+    btnClk[i].addEventListener('click', changeColor);
+}
