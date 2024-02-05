b59375c246730cb0608f350c720c624399821b8e
gamzazoha <70256365+gamzazoha@users.noreply.github.com>
2024-02-05 21:35:08 +0900

    Add files via upload

---
+++ assignment2/main.js
@@ -0,0 +1,17 @@
+// get data
+
+$.ajax({
+    url: "https://shopping-mall-rzdwe.run.goorm.site/books/",
+    method: "GET",
+    // async: true, /* ??? */
+    success : function(result) {
+        console.log(result);
+        for(let i=0;i<result.length;i++){
+            // $(".product").eq(i).find("img").attr("src", "https://shopping-mall-rzdwe.run.goorm.site/books/image/"+result[i].imageNum+".jpeg");
+            $(".product").eq(i).find("img").attr("src", result[i].imageNum);
+            $(".product").eq(i).find(".title").text(result[i].title);
+            $(".product").eq(i).find(".author").text("저자 : "+result[i].author);
+            $(".product").eq(i).find(".price").text(result[i].price+"₩");
+        }
+    }
+});
