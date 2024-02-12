7c1147ab00bfa9ebc16f79b3c2598f49ba7510c4
gamzazoha <70256365+gamzazoha@users.noreply.github.com>
2024-02-12 16:49:52 +0900

    Add files via upload

--- assignment2/addProduct.js
+++ assignment2/addProduct.js
@@ -6,10 +6,10 @@ let price = 0;
 for(let i=0;i<window.localStorage.length;i++){
     const key = window.localStorage.key(i);
     
-    const newLi = document.createElement("li");
-    newLi.className = "item";
     const divNode = document.createElement("div");
     divNode.className = "product-box";
+    const newLi = document.createElement("li");
+    newLi.className = "item";
     const newBtn = document.createElement("button");
     newBtn.className = "delete";
     const newIcon = document.createElement("span");
@@ -26,24 +26,34 @@ for(let i=0;i<window.localStorage.length;i++){
     const tmp = info.price;
     price = price + Number(tmp.substring(1,));
     newLi.textContent = info.title + ", " + info.author + ", " + info.price;
+    
+    divNode.classList = divNode.classList + " " + key;
+
     if(i == window.localStorage.length - 1){
         priceP.textContent = price + "원";
     }
 };
 
-// const deleteBtn = document.getElementsByClassName("delete");
-// const parentUl = document.getElementById("item-box");
-// const childLi = document.getElementsByClassName("item");
+const deleteBtn = document.getElementsByClassName("delete");
+const parentUl = document.getElementById("item-box");
+const childLi = document.getElementsByClassName("item");
 
-// function Delete() {
-//     const parentDiv = this.parent.parent;
-//     parentDiv.remove;
-// };
+function Delete() {
+    const parentDiv = this.parentElement;
+    const keyValue = parentDiv.classList[1];
+    const info = JSON.parse(localStorage.getItem(keyValue));
+    const delPrice = Number(info.price.substring(1,));
 
-// parentUl.addEventListener(("mousemove"), (e) => {
-//     console.log(childLi.length);
-//     for(let i=0;i<=childLi.length;i++){
-//         console.log(e.currentTarget.children.children);
-//         // e.currentTarget.children.children.next.addEventListener("click", Delete);
-//     }
-// });
+    priceP.textContent = (price - delPrice) + "원";
+    window.localStorage.removeItem(keyValue);
+    parentDiv.remove();
+};
+
+parentUl.addEventListener(("mousemove"), (e) => {
+    for(let i=0;i<childLi.length;i++){
+        const key = window.localStorage.key(i);
+        const childElement = e.currentTarget.children[i].querySelector(".delete");
+
+        childElement.addEventListener("click", Delete);
+    }
+});
